f7e94ae60a0b6f131dab906ff8591f187744209c
her_username <nahnaveed501@gmail.com>
2024-05-19 19:53:22 +0500

    Text arranging app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-
 import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "Black";
+      showAlert("Dark mode enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+
   return (
     <>
-        <Navbar title="Shopie" home="Docx"/>
-        <div className="container ">
-        <Textarea heading="Enter your Text "/>
-
+      <Router>
+        <Navbar title="Shopie" home="Home" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<Textarea showAlert={showAlert} heading="Enter your Text" mode={mode} />} />
+          </Routes>
         </div>
+      </Router>
     </>
   );
 }
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -7,12 +7,17 @@ export default function Textarea(props) {
     const handleUPclick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+       
+            props.showAlert("Convert To UpperCase","success");
+
+    
     }
 
 
     const handleLOclick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Convert To LowerCase","success");
     }
 
 
@@ -28,13 +33,21 @@ export default function Textarea(props) {
         })
         .join(" ");
         setText(newText);
+        props.showAlert("Convert To First Letter Capital","success");
     }
     const handleCopyclick = () => {
         navigator.clipboard.writeText(text);
+        props.showAlert("Copy To ClipBoard","success");
+    }
+    const handleRemoveSpace = () => {
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Remove Extra Spaces","success");
     }
     const handleClearclick = () => {
         let newText = '';
         setText(newText);
+        props.showAlert("Clear All text","success");
     }
    
 
@@ -44,23 +57,24 @@ export default function Textarea(props) {
 
     return (
         <>
-        <div className='container my-3'>
+        <div className='container my-3' style={{ color:props.mode === 'dark' ? 'white' : 'black' }}>
             <h1>{props.heading}</h1>
             <div className="form-floating">
-                <textarea className="form-control" placeholder="Textarea" id="floatingTextarea2" value={text} onChange={handleOnChange} style={{ height: '200px' }}></textarea>
+                <textarea className="form-control" placeholder="Textarea" id="floatingTextarea2" value={text} onChange={handleOnChange} style={{ height: '200px',backgroundColor:props.mode === 'dark' ? 'black' : 'white',color:props.mode === 'dark' ? 'white' : 'black' }}></textarea>
 
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUPclick} style={{ marginTop: '8px' }}>Convert To UpperCase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLOclick} style={{ marginTop: '8px' }}>Convert To LowerCase</button>
-            <button className="btn btn-primary mx-2" onClick={handleFLclick} style={{ marginTop: '8px' }}>Capital First Letter</button>
-            <button className="btn btn-primary mx-2" onClick={handleCopyclick} style={{ marginTop: '8px' }}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUPclick} style={{ marginTop: '8px' }}>Convert To UpperCase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLOclick} style={{ marginTop: '8px' }}>Convert To LowerCase</button>
+            <button  disabled={text.length===0}className="btn btn-primary mx-2 my-1" onClick={handleFLclick} style={{ marginTop: '8px' }}>Capital First Letter</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopyclick} style={{ marginTop: '8px' }}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleRemoveSpace} style={{ marginTop: '8px' }}>Remove Spaces</button>
             
-            <button className="btn btn-primary mx-2" onClick={handleClearclick} style={{ marginTop: '8px' }}>Clear</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearclick} style={{ marginTop: '8px' }}>Clear</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{ color:props.mode === 'dark' ? 'white' : 'black' }}>
             <h2>Your Text Summary</h2>
-            <p>{text ? text.split(" ").length : 0} words and {text.length} characters</p>
-            <p>{0.008*(text ? text.split(" ").length:0)} Minutes Read </p>
+            <p>{ text.split(/\s+/).filter((element)=>{return element.length!==0}).length } words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").length} Minutes Read </p>
             <h2>Preview</h2>
             <p>{text}</p>
            
